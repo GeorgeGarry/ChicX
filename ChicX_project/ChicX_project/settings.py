2d23b35fa19eb7d4fdf3b0ef8f5baff139430a06
GeorgeGarry <leynovyegor@gmail.com>
2024-06-11 19:00:49 -0600

    added allowed hosts

--- ChicX_project/ChicX_project/settings.py
+++ ChicX_project/ChicX_project/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-74h17199^nh3-7_wdls5@q*)9xl)w=6ml&*wj-l(9e*@0b-dle
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["chicx.onrender.com"]
 
 # Application definition
 
