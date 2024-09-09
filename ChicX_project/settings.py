e99cb8e2b2f3d2603ce970f81534fe2af37e4fd1
GeorgeGarry <leynovyegor@gmail.com>
2024-09-09 14:30:35 -0600

    added whitenoise middleware

--- ChicX_project/settings.py
+++ ChicX_project/settings.py
@@ -46,7 +46,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'ChicX_project.urls'
 
